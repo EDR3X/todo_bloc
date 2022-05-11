f0d63b083fe2a2e6a8c79a4660dfbb7b84452ae3
EDR3X <dhunganaannanta@gmail.com>
2022-05-12 01:56:56 +0545

    corrections

--- lib/pages/todo_page/show_todos.dart
+++ lib/pages/todo_page/show_todos.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_bloc/cubits/cubits.dart';
+import 'package:todo_bloc/blocs/blocs.dart';
 import 'package:todo_bloc/models/todo_model.dart';
 
 class ShowTodos extends StatelessWidget {
@@ -8,7 +8,7 @@ class ShowTodos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final todos = context.watch<FilteredTodosCubit>().state.filteredTodos;
+    final todos = context.watch<FilteredTodosBloc>().state.filteredTodos;
     return ListView.separated(
       primary: false,
       shrinkWrap: true,
@@ -24,7 +24,9 @@ class ShowTodos extends StatelessWidget {
           background: showBackground(0),
           secondaryBackground: showBackground(1),
           onDismissed: (_) {
-            context.read<TodoListCubit>().removeTodo(todos[index]);
+            context
+                .read<TodoListBloc>()
+                .add(RemoveTodoEvent(todo: todos[index]));
           },
           confirmDismiss: (_) => showDialog(
             context: context,
@@ -120,9 +122,9 @@ class _TodoItemState extends State<TodoItem> {
                       onPressed: () => setState(() {
                         _error = textController.text.isEmpty ? true : false;
                         if (!_error) {
-                          context
-                              .read<TodoListCubit>()
-                              .editTodo(widget.todo.id, textController.text);
+                          context.read<TodoListBloc>().add(EditTodoEvent(
+                              id: widget.todo.id,
+                              todoDesc: textController.text));
                           Navigator.pop(context);
                         }
                       }),
@@ -136,7 +138,7 @@ class _TodoItemState extends State<TodoItem> {
       leading: Checkbox(
         value: widget.todo.completed,
         onChanged: (bool? checked) {
-          context.read<TodoListCubit>().toggleTodo(widget.todo.id);
+          context.read<TodoListBloc>().toggleTodo(widget.todo.id);
         },
       ),
       title: Text(widget.todo.desc),
